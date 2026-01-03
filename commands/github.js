daf8a93450c9adbd5ed3d3d0c449fabdbc1f4b50
PRINCE TECH <majidiabduli2003@gmail.com>
2026-01-03 12:26:00 +0300

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/Princeton2003/PRINCE-XMD');
+    const res = await fetch('https://api.github.com/repos/Princeton2003/PRINCE--XMD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
